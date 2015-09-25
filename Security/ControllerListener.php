4b38659f31e7381de1d98a826113f8549307be80
Ahmed Mohamed <a.mohamed@edfa3ly.com.eg>
2015-09-25 04:10:57 +0200

    Ammend listener to fit our needs

--- Security/ControllerListener.php
+++ Security/ControllerListener.php
@@ -2,6 +2,7 @@
 namespace Lsw\SecureControllerBundle\Security;
 
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Util\ClassUtils;
 use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -13,55 +14,70 @@ class ControllerListener
 {
     private $annotationReader;
     private $securityContext;
-    
-    public function __construct(Reader $annotationReader, SecurityContextInterface $securityContext)
+    private $router;
+
+    public function __construct(
+        Reader $annotationReader,
+        SecurityContextInterface $securityContext,
+        RouterInterface $router)
     {
-      $this->annotationReader = $annotationReader;
-      $this->securityContext = $securityContext;
+        $this->annotationReader = $annotationReader;
+        $this->securityContext  = $securityContext;
+        $this->router           = $router;
     }
-  
+
     public function onKernelController(FilterControllerEvent $event)
     {
+        $route = $this->router->match($event->getRequest()->getPathInfo());
+        if (strpos($route['_controller'], 'BackendBundle') !== false) {
+            if (!$this->securityContext->isGranted('ROLE_ADMIN')) {
+                throw new AccessDeniedException(
+                    'Current user is not granted required role "' . $role . '".'
+                );
+            }
+        }
+
         $controller = $event->getController();
-        
+
         list($object, $method) = $controller;
-        
+
         // the controller could be a proxy, e.g. when using the JMSSecurityExtraBundle or JMSDiExtraBundle
         $className = ClassUtils::getRealClass($object);
-        
-        $reflectionClass = new \ReflectionClass($className);
+
+        $reflectionClass  = new \ReflectionClass($className);
         $reflectionMethod = $reflectionClass->getMethod($method);
 
-        $classAnnotations = $this->annotationReader->getClassAnnotations($reflectionClass);
+        $classAnnotations   = $this->annotationReader->getClassAnnotations($reflectionClass);
         $methodsAnnotations = $this->annotationReader->getMethodAnnotations($reflectionMethod);
 
-        $allAnnotations = array_merge($classAnnotations,$methodsAnnotations);
-        
-        $secureAnnotations = array_filter($allAnnotations, function($annotation) {
-            return $annotation instanceof Secure;
-        });
-        
+        $allAnnotations = array_merge($classAnnotations, $methodsAnnotations);
+
+        $secureAnnotations = array_filter($allAnnotations,
+            function ($annotation) {
+                return $annotation instanceof Secure;
+            }
+        );
+
         foreach ($secureAnnotations as $secureAnnotation) {
             if (!$this->securityContext->getToken()) {
-              $filename = $reflectionClass->getFileName();
-              throw new AuthenticationCredentialsNotFoundException(
-                  '@Secure(...) annotation found without firewall on "'.$method.'" in "'.$filename.'"'
-              );
+                $filename = $reflectionClass->getFileName();
+                throw new AuthenticationCredentialsNotFoundException(
+                    '@Secure(...) annotation found without firewall on "' . $method . '" in "' . $filename . '"'
+                );
             }
-            $roles = explode(',',$secureAnnotation->roles);
-            foreach ($roles as $role)
-            {   
+            $roles = explode(',', $secureAnnotation->roles);
+            foreach ($roles as $role) {
                 $role = trim($role);
-                       
-                if (!$role) continue;
+
+                if (!$role) {
+                    continue;
+                }
                 if (!$this->securityContext->isGranted($role)) {
                     throw new AccessDeniedException(
-                        'Current user is not granted required role "'.$role.'".'
+                        'Current user is not granted required role "' . $role . '".'
                     );
                 }
             }
-            
         }
     }
-    
 }
